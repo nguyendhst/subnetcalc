67b75ab18a164a1b24c425a9b715320b3ddd5a3c
nguyendhst <mountngubinh@gmail.com>
2022-07-08 22:32:34 +0700

    ipv6 logic done(kinda)

--- calc/calc.go
+++ calc/calc.go
@@ -2,7 +2,6 @@ package calc
 
 import (
 	"fmt"
-	_ "math/big"
 	"regexp"
 	"strconv"
 	"strings"
@@ -48,9 +47,8 @@ type IPv4Result struct {
 }
 
 type IPv6Result struct {
-	Address     string `json:"address"`
-	FullAddress string `json:"full_address"`
-	TotalHost   int    `json:"total_host"`
+	Address        string `json:"address"`
+	NetworkAddress string `json:"network_address"`
 }
 
 func (ipv4 *IPv4) Process() (IPResult, error) {
@@ -62,7 +60,7 @@ func (ipv4 *IPv4) Process() (IPResult, error) {
 		return IPv4Result{}, fmt.Errorf("invalid IPv4 address")
 	}
 	addr, k := Parse(ipv4.Addr)
-	if k < 0 {
+	if k <= 0 {
 		return IPv4Result{}, fmt.Errorf("problem with parsing")
 	}
 	return CalcV4(addr, k), nil
@@ -94,7 +92,7 @@ func (ipv6 *IPv6) Process() (IPResult, error) {
 		return IPv6Result{}, fmt.Errorf("invalid IPv6 address")
 	}
 	addr, k := Parse(ipv6.Addr)
-	if k < 0 {
+	if k <= 0 {
 		return IPv6Result{}, fmt.Errorf("problem with parsing")
 	}
 	return CalcV6(addr, k), nil
@@ -149,7 +147,7 @@ func maskV4(addr string, prefix int) []string {
 	third := mask >> 8 & 0xff
 	fourth := mask & 0xff
 
-	arr := ipToSlice(addr)
+	arr := ip4ToSlice(addr)
 
 	return []string{
 		fmt.Sprintf("%03d.%03d.%03d.%03d", first, second, third, fourth),
@@ -161,31 +159,51 @@ func maskV4(addr string, prefix int) []string {
 	}
 }
 
-func ipToSlice(address string) []int {
+func ip4ToSlice(address string) []uint64 {
 	ip := strings.Split(address, ".")
-	result := make([]int, 4)
+	result := make([]uint64, 4)
 	for i, v := range ip {
-		result[i], _ = strconv.Atoi(v)
+		result[i], _ = strconv.ParseUint(v, 10, 8)
 	}
 	return result
 }
 
 func CalcV6(address string, prefix int) IPResult {
+	arr := func(address string) []uint64 {
+		ip := strings.Split(address, ":")
+		result := make([]uint64, 8)
+		for i, v := range ip {
+			result[i], _ = strconv.ParseUint(v, 16, 16)
+		}
+		return result
+	}(address)
+
+	block := 7 - ((127 - prefix) / 16)
+	bitNum := 15 - ((127 - prefix) % 16)
+
+	res := func(arr []uint64, block int, bit int) []string {
+		temp := uint64((arr[block] >> uint64(bit)) << uint64(bit))
+		fullip := fmt.Sprintf("%04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7])
+		arr[block] = temp
+		for i := 7; i > block; i-- {
+			arr[i] = 0
+		}
+		return []string{
+			fullip,
+			fmt.Sprintf("%04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7]),
+		}
+	}(arr, block, bitNum)
+
 	return IPv6Result{
-		Address: address,
+		Address:        res[0],
+		NetworkAddress: res[1],
 	}
 }
 
 func (ipv4 IPv4Result) String() {
-	fmt.Printf("IPv4: \t\t\t%s/%d\n", ipv4.Address, ipv4.Prefix)
-	fmt.Printf("Mask: \t\t\t%s\n", ipv4.Mask)
-	fmt.Printf("Mask Binary: \t\t%s\n", ipv4.MaskBinary)
-	fmt.Printf("Wildcard: \t\t%s\n", ipv4.Wildcard)
-	fmt.Printf("Wildcard Binary: \t%s\n", ipv4.WildcardBinary)
-	fmt.Printf("Lower: \t\t\t%s\n", ipv4.Lower)
-	fmt.Printf("Upper: \t\t\t%s\n", ipv4.Upper)
+	fmt.Printf("IPv4: \t\t\t%s/%d\nMask: \t\t\t%s\nMask Binary: \t\t%s\nWildcard: \t\t%s\nWildcard Binary: \t%s\nLower Bound: \t\t%s\nUpper Bound: \t\t%s\n", ipv4.Address, ipv4.Prefix, ipv4.Mask, ipv4.MaskBinary, ipv4.Wildcard, ipv4.WildcardBinary, ipv4.Lower, ipv4.Upper)
 }
 
 func (ipv6 IPv6Result) String() {
-	fmt.Printf("IPv6: %s/%s\n", ipv6.Address, ipv6.FullAddress)
+	fmt.Printf("IPv6: \t\t%s\nNetwork: \t%s\n", ipv6.Address, ipv6.NetworkAddress)
 }
